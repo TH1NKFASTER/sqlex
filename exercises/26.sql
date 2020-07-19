392ab093d1857e2d363bee2928b146120dd54425
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-07-19 19:05:44 +0300

    Create 26.sql

---
+++ exercises/26.sql
@@ -0,0 +1,14 @@
+SELECT AVG(price) 
+  FROM (SELECT price 
+          FROM Product 
+              INNER JOIN Laptop 
+              ON Laptop.model = Product.model 
+         WHERE maker = 'A'
+
+UNION ALL
+
+SELECT price 
+  FROM Product 
+      INNER JOIN PC 
+      ON PC.model = Product.model 
+ WHERE maker = 'A') AS x
