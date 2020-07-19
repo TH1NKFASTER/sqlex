17b2c98155c060813beeb438b249e8a363b97249
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-07-19 18:22:17 +0300

    Update 7.sql

--- exercises/7.sql
+++ exercises/7.sql
@@ -1,21 +1,21 @@
 SELECT DISTINCT Product.model AS model, price
-FROM Product 
-    INNER JOIN PC
-    ON Product.model = PC.model 
+  FROM Product
+      INNER JOIN PC
+      ON Product.model = PC.model
+ WHERE maker LIKE '%B%'
+
+UNION
+
+SELECT DISTINCT Product.model AS model, price
+  FROM Product
+      INNER JOIN Laptop
+      ON Product.model = Laptop.model
 WHERE maker LIKE '%B%'
 
 UNION
 
-SELECT DISTINCT Product.model AS model, price 
-FROM Product 
-    INNER JOIN Laptop 
-    ON Product.model = Laptop.model
-WHERE maker LIKE '%B%'
-
-Union
-
-SELECT DISTINCT Product.model AS model, price 
-FROM Product 
-    INNER JOIN Printer 
-    ON Product.model = Printer.model 
-WHERE maker LIKE '%B%'
+SELECT DISTINCT Product.model AS model, price
+  FROM Product
+      INNER JOIN Printer
+      ON Product.model = Printer.model
+ WHERE maker LIKE '%B%'
