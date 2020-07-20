8eda362bd848ca2e0b86d5306ae23fe7a05ff09a
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2020-07-20 18:56:43 +0300

    Create 50.sql

---
+++ exercises/50.sql
@@ -0,0 +1,9 @@
+SELECT battle 
+  FROM Outcomes 
+      INNER JOIN Ships 
+      ON class = 'Kongo' 
+      AND ship = nameSELECT battle 
+  FROM Outcomes 
+      INNER JOIN Ships 
+      ON class = 'Kongo' 
+      AND ship = name
